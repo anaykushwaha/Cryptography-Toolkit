812435ec8a2644ed801d2cf3631452ce5ed005a0
Anay Kushwaha <anaykushwaha@gmail.com>
2026-08-06 16:57:45 +0530

    Data directory initialized

---
+++ data/__init__.py
@@ -0,0 +1,27 @@
+# __init__.py
+# Data package for the entire
+# Cryptography Toolkit
+
+# Contains reference data
+# used throughout the project,
+# including English language
+# statistics and sample text
+
+# Resources
+# english_frequency.json - Letter frequencies
+# english_words.txt - English dictionary
+# common_bigrams.txt - Common bigrams
+# common_trigrams.txt - Common trigrams
+# sample_messages.txt - Example plaintext
+# stopwords.txt - English stopwords
+
+
+__all__ = [
+    "english_frequency",
+    "english_words",
+    "common_bigrams",
+    "common_trigrams",
+    "sample_messages",
+    "stopwords",
+] 
+
