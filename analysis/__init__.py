20edea803f9b9c364a4a54c89a5b9221582299c3
Anay Kushwaha <anaykushwaha@gmail.com>
2026-08-09 17:25:49 +0530

    Analysis directory initialization modified

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,4 +1,5 @@
 # __init__.py
+
 # Cryptanalysis package for the entire Cryptography Toolkit
 
 # Contains frequency analysis, statistical analysis,
@@ -6,33 +7,65 @@
 # n-gram analysis, and scoring utilities
 
 # Modules
+
 # brute_force - Automatic Caesar Cipher cracking
+
 # english_data - English language reference data
+
 # entropy - Shannon entropy calculations
+
 # frequency - Letter frequency analysis
+
 # ioc - Index of Coincidence calculations
+
 # ngrams - Bigram and trigram analysis
+
 # scorer - Candidate scoring utilities
+
 # statistics - Statistical helper functions
 
 
 from .frequency import *
+
 from .statistics import *
+
 from .entropy import *
+
 from .ioc import *
+
 from .ngrams import *
+
 from .brute_force import *
+
 from .scorer import *
+
 from .english_data import *
 
+
 __all__ = [
+
+    # Frequency
     "frequency",
+
+    # Statistics
     "statistics",
+
+    # Entropy
     "entropy",
+
+    # IOC
     "ioc",
+
+    # N-Grams
     "ngrams",
+
+    # Brute Force
     "brute_force",
+
+    # Scorer
     "scorer",
+
+    # English Data
     "english_data",
 ] 
 
