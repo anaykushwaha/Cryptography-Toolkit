0b47a46e96efdd265682d2973afbdd0f279592ca
Anay Kushwaha <anaykushwaha@gmail.com>
2026-08-10 20:29:08 +0530

    Tests directory initialized

---
+++ tests/__init__.py
@@ -0,0 +1,29 @@
+# __init__.py
+# Test suite package
+# for the entire Cryptography Toolkit
+
+# Contains unit tests
+# covering encryption,
+# analysis, file handling,
+# and user interfaces
+
+# Modules
+# test_caesar - Caesar Cipher tests
+# test_chain - Chain Cipher tests
+# test_rot - ROT Cipher tests
+# test_frequency - Frequency analysis tests
+# test_statistics - Statistics tests
+# test_entropy - Entropy tests
+# test_fileio - File handling tests
+
+
+__all__ = [
+    "test_caesar",
+    "test_chain",
+    "test_rot",
+    "test_frequency",
+    "test_statistics",
+    "test_entropy",
+    "test_fileio",
+] 
+
