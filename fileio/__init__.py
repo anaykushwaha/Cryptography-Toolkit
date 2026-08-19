389249c71c8943eaef6fa4e7fd5b4f703a323094
Anay Kushwaha <anaykushwaha@gmail.com>
2026-08-19 21:16:23 +0530

    Fileio directory initialization modified

--- fileio/__init__.py
+++ fileio/__init__.py
@@ -1,26 +1,98 @@
 # __init__.py
-# File handling package for the entire Cryptography Toolkit
+
+# File handling package for the entire
+# Cryptography Toolkit
 
 # Contains utilities for reading, writing,
 # encrypting, decrypting, exporting,
 # backing up, and tracking files
 
+
 # Modules
-# file_manager - File encryption and decryption
-# history - Encryption history management
-# exporters - Export analysis reports
-# backups - Backup utilities
+# file_manager - Core file management,
+#                reading, writing, copying,
+#                deleting, and file operations
+#
+# history - Encryption and operation
+#           history management
+#
+# exporters - Export utilities for analysis
+#             results, history, and candidates
+#
+# backups - Backup creation, restoration,
+#           deletion, and metadata management
 
 
+# File Manager
 from .file_manager import *
+
+
+# History
 from .history import *
+
+
+# Exporters
 from .exporters import *
+
+
+# Backups
 from .backups import *
 
-__all__ = [
-    "file_manager",
-    "history",
-    "exporters",
-    "backups",
-] 
 
+__all__ = [
+    # File Manager
+    "FileManager",
+    "FileManagerError",
+    "FileNotFoundError",
+    "FileAlreadyExistsError",
+    "InvalidFileError",
+    "FileOperationError",
+
+    # History
+    "HistoryError",
+    "InvalidHistoryEntryError",
+    "HistoryStorageError",
+    "HistoryEntry",
+    "HistoryManager",
+    "get_history_manager",
+    "record_operation",
+    "save_history",
+    "load_history",
+    "get_history",
+    "clear_history",
+    "verify_entry",
+
+    # Exporters
+    "ExportError",
+    "UnsupportedFormatError",
+    "InvalidExportDataError",
+    "ExportOperationError",
+    "ExportManager",
+    "get_export_manager",
+    "export_text",
+    "export_json",
+    "export_csv",
+    "export_history",
+    "export_analysis",
+    "export_candidates",
+    "export",
+    "verify_export_format",
+    "verify_output_path",
+
+    # Backups
+    "BackupError",
+    "BackupNotFoundError",
+    "BackupAlreadyExistsError",
+    "InvalidBackupError",
+    "BackupOperationError",
+    "BackupMetadata",
+    "BackupManager",
+    "get_backup_manager",
+    "create_backup",
+    "restore_backup",
+    "delete_backup",
+    "list_backups",
+    "save_backups",
+    "load_backups",
+    "verify_backup",
+]
